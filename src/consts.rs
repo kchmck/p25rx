9d5b61d5849bfcdc4a192521a557505fa08d28e8
Mick Koch <mick@kochm.co>
2016-08-14 10:08:28 -0400

    Add BUF_SIZE_COMPLEX constant

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,6 @@
 pub const BUF_COUNT: usize = 16;
 pub const BUF_SIZE_RAW: usize = 32768;
+pub const BUF_SIZE_COMPLEX: usize = BUF_SIZE_RAW / 2;
 
 pub const SDR_SAMPLE_RATE: u32 = 240_000;
 pub const BASEBAND_SAMPLE_RATE: u32 = 48000;
